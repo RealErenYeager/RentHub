068758103bab7573ccfbf6abb73a8c09c5ad06af
RealErenYeager <realerenyeager2022@gmail.com>
2025-04-11 15:59:04 +0530

    owner login and admin login FIX bug

--- admin-engine.php
+++ admin-engine.php
@@ -13,10 +13,10 @@ $password='';
 
 $errors=array();
 
-$db = new mysqli('localhost','root','','renthouse');
+$db = new mysqli("sql312.infinityfree.com", "if0_38686514", "IPP2Lin08rt", "if0_38686514_renthub");
 
-if($db->connect_error){
-	echo "Error connecting database";
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
 }
 
 if(isset($_POST['admin_login'])){
--- owner-engine.php
+++ owner-engine.php
@@ -18,10 +18,10 @@ $id_photo='';
 
 $errors=array();
 
-$db = new mysqli('localhost','root','','renthouse');
+$db = new mysqli("sql312.infinityfree.com", "if0_38686514", "IPP2Lin08rt", "if0_38686514_renthub");
 
-if($db->connect_error){
-	echo "Error connecting database";
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
 }
 
 
