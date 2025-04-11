25350341a8362eda9c1f4411d6ebb998dbb4c91b
RealErenYeager <realerenyeager2022@gmail.com>
2025-04-11 16:12:11 +0530

    engine.php localhost to root updated

--- owner/engine.php
+++ owner/engine.php
@@ -24,10 +24,10 @@ $owner_id='';
 
 
 
-$db = new mysqli('localhost','root','','renthouse');
+$db = new mysqli("sql312.infinityfree.com", "if0_38686514", "IPP2Lin08rt", "if0_38686514_renthub");
 
-if($db->connect_error){
-	echo "Error connecting database";
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
 }
 
 if(isset($_POST['add_property'])){
