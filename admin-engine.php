aa07b1a7e3f2b03eedc2e0d822acdea9d774d2e6
RealErenYeager <realerenyeager2022@gmail.com>
2025-04-11 15:43:12 +0530

    replace all session_start code FIX bug

--- admin-engine.php
+++ admin-engine.php
@@ -2,7 +2,10 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 
 $admin_id='';
 $email='';
--- chatpage.php
+++ chatpage.php
@@ -1,5 +1,8 @@
 <?php 
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+  session_start();
+}
+
 isset($_SESSION["email"]);
 include("navbar.php");
 include("config/config.php");
